49a2cf5d0724fcc45d0708374af7422bc6f32092
jsass <jssass@gmail.com>
2023-08-23 06:11:48 -0500

    bizpy: rework core classes

--- bizpy/finance.py
+++ bizpy/finance.py
@@ -2,108 +2,170 @@
 
 import abc
 import datetime
-import pandas
+import pandas as pd
+import uuid
 
+from collections import UserDict
 from dataclasses import dataclass, field
-from datetime import date, timedelta
-from typing import List, NamedTuple, Optional
+from enum import IntEnum
+from typing import List, NamedTuple, Optional, Tuple
 
 SECONDS_PER_YEAR = 31536000
 
-@dataclass
-class Account(abc.ABC):
-    value: float = 0
 
-    @abc.abstractmethod
-    def debit(self, amount: float):
-        pass
+class AccountType(IntEnum):
+    # Current Assets
+    CASH = 100
+    INVENTORY = 101
+    ACCOUNTS_RECEIVABLE = 102
 
-    @abc.abstractmethod
-    def credit(self, amount: float):
-        pass
+    # Current Liabilities
+    SHAREHOLDERS_EQUITY = 300
+    ACCOUNTS_PAYABLE = 301
 
-class AssetAccount(Account):
-    def debit(self, amount: float):
-        self.value += amount
 
-    def credit(self, amount: float):
-        self.value -= amount
-
-@dataclass
-class LiabilityAccount(Account):
-    def debit(self, amount: float):
-        self.value -= amount
-
-    def credit(self, amount: float):
-        self.value += amount
+class TransactionType(IntEnum):
+    DEBIT = 0
+    CREDIT = 1
 
 
 @dataclass
 class Transaction:
-    date: date
+    id: int
+    entry_id: int
+    date: datetime.date
+    type: TransactionType
+    account: AccountType
     amount: float
-    debit_account: Account
-    credit_account: Account
 
-    def pv(self, discount_rate, current_date: Optional[date] = None):
-        if current_date is None:
-            current_datetime = date.today()
+    def __le__(self, other):
+        return self.date <= other.date
 
-        years = (self.date - current_date.total_seconds()) / SECONDS_PER_YEAR
+    def __lt__(self, other):
+        return self.date < other.date
 
-        return self.amount / (1 + discount_rate) ** years
 
 @dataclass
-class Ledger:
-    # Asset Accounts
-    inventory: AssetAccount = field(default_factory=AssetAccount)
-    cash: AssetAccount = field(default_factory=AssetAccount)
+class Account(abc.ABC):
+    name: str
 
-    # Liability Accounts
-    shareholders_equity: LiabilityAccount = field(default_factory=LiabilityAccount)
-    accounts_payable: LiabilityAccount = field(default_factory=LiabilityAccount)
+    def __post_init__(self):
+        self._transactions: List[Transaction] = []
+
+    def post(self, transaction: Transaction):
+        self._transactions.append(transaction)
+
+    def _sum_transactions(self, type: TransactionType, date: Optional[datetime.date] = None):
+        date = datetime.date.today() if date is None else date
+
+        return sum(
+            [
+                transaction.amount
+                for transaction in self._transactions
+                if (transaction.date <= date) and (transaction.type == type)
+            ]
+        )
+
+    @abc.abstractmethod
+    def get_balance(self, date: Optional[datetime.date]):
+        pass
+
+
+class AssetAccount(Account):
+    def get_balance(self, date: Optional[datetime.date] = None):
+        return self._sum_transactions(TransactionType.DEBIT, date) - self._sum_transactions(
+            TransactionType.CREDIT, date
+        )
+
+
+class LiabilityAccount(Account):
+    def get_balance(self, date: Optional[datetime.date] = None):
+        return self._sum_transactions(TransactionType.CREDIT, date) - self._sum_transactions(
+            TransactionType.DEBIT, date
+        )
+
 
 class Business:
     def __init__(self):
-        self._ledger: Ledger = Ledger()
         self._shares = 0
 
-        # Transactions that have been processed.
-        self._journal = []
+        self._ledger = {
+            AccountType.CASH: AssetAccount("cash"),
+            AccountType.INVENTORY: AssetAccount("inventory"),
+            AccountType.ACCOUNTS_PAYABLE: LiabilityAccount("payable"),
+            AccountType.ACCOUNTS_RECEIVABLE: AssetAccount("receivable"),
+            AccountType.SHAREHOLDERS_EQUITY: LiabilityAccount("shareholders_equity"),
+        }
 
-    def raise_equity(self, shares, amount: float, date: datetime.date):
+        self._transactions = []
+
+        # Operations
+        self._unit_price = 100
+        self._unit_cogs = 40
+
+    def raise_equity(self, shares, price_per_share: float, date: datetime.date):
         self._shares += shares
-        self._journal.append(
+        entry_id = uuid.uuid4()
+        self._transactions.append(
             Transaction(
-                date,
-                amount, 
-                self._ledger.cash,
-                self._ledger.shareholders_equity,
+                uuid.uuid4().int,
+                entry_id.int,
+                datetime.date.today(),
+                TransactionType.DEBIT,
+                AccountType.CASH,
+                shares * price_per_share,
+            )
+        )
+        self._transactions.append(
+            Transaction(
+                uuid.uuid4().int,
+                entry_id.int,
+                datetime.date.today(),
+                TransactionType.CREDIT,
+                AccountType.SHAREHOLDERS_EQUITY,
+                shares * price_per_share,
             )
         )
 
-    def purchase_inventory(self, amount: float, purchase_date: datetime, terms: timedelta):
-        self._journal.append(
+    def buy_inventory(self, amount: float):
+        entry_id = uuid.uuid4().int
+        self._transactions.append(
             Transaction(
-                purchase_date,
+                uuid.uuid4().int,
+                entry_id,
+                datetime.date.today(),
+                TransactionType.DEBIT,
+                AccountType.INVENTORY,
                 amount,
-                self._ledger.inventory,
-                self._ledger.accounts_payable,
             )
         )
-
-        self._journal.append(
+        self._transactions.append(
             Transaction(
-                purchase_date + terms,
+                uuid.uuid4().int,
+                entry_id,
+                datetime.date.today(),
+                TransactionType.CREDIT,
+                AccountType.ACCOUNTS_PAYABLE,
                 amount,
-                self._ledger.accounts_payable,
-                self._ledger.cash,
             )
         )
 
-    def process(self, date: Optional[datetime.date] = None):
-        for transaction in self._journal:
-            transaction.debit_account.debit(transaction.amount)
-            transaction.credit_account.credit(transaction.amount)
+    def process_transactions(self):
+        for transaction in self._transactions:
+            self._ledger[transaction.account].post(transaction)
 
-        self._journal = []
+
+def main():
+    business = Business()
+    business.raise_equity(50000, 1, datetime.date.today())
+    business.raise_equity(150000, 10, datetime.date.today())
+    business.buy_inventory(10000)
+
+    business.process_transactions()
+
+    for account_id, account in business._ledger.items():
+        print(account_id.name, account.get_balance())
+
+
+if __name__ == "__main__":
+    main()
