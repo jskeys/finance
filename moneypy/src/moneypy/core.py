d62d81875f88aa43ae0c3057084b99ced1abe56d
jsass <jssass@gmail.com>
2026-03-08 21:12:52 -0500

    moneypy: add to_decimal(..); add constants

--- moneypy/src/moneypy/core.py
+++ moneypy/src/moneypy/core.py
@@ -4,6 +4,17 @@ from __future__ import annotations
 
 import numpy as np
 import numpy.typing as npt
+import typing
+from dateutil.relativedelta import relativedelta
+from decimal import Decimal, ROUND_HALF_EVEN
+
+
+CURRENCY_EPSILON = Decimal("1.00")
+ONE_YEAR = relativedelta(years=1)
+ROUNDING_STRATEGY = ROUND_HALF_EVEN
+ZERO = Decimal(0)
+
+DecimalLike = typing.Union[int, float, str, Decimal]
 
 
 def calc_annuity(
@@ -27,3 +38,10 @@ def calc_annuity(
         1 / periods,
         rate / (1 - np.power(1 + rate, -periods)),
     )
+
+
+def to_decimal(value: DecimalLike):
+    if not isinstance(value, Decimal):
+        value = Decimal(str(value))
+
+    return value.quantize(CURRENCY_EPSILON, rounding=ROUNDING_STRATEGY)
