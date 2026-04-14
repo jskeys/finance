a81eabc7a9155f5306a4f74c8d3a20ea1e33d28e
jon <donny.creamer@gmail.com>
2026-04-14 06:57:22 -0500

    moneypy: use meshes in mortgage explorer

--- moneypy/src/moneypy/apps/mortgage_tool_sl.py
+++ moneypy/src/moneypy/apps/mortgage_tool_sl.py
@@ -1,5 +1,4 @@
 import dataclasses
-from enum import IntEnum, auto
 from typing import Callable, Optional
 
 import numpy as np
@@ -7,7 +6,12 @@ import plotly.colors as pc
 import plotly.graph_objects as go
 import streamlit as streamlit
 
-import moneypy.mortgage as mortgage
+from moneypy.mortgage import (
+    MortgageParameters,
+    calc_down_payment,
+    calc_home_value,
+    calc_monthly_payment,
+)
 
 
 @dataclasses.dataclass(frozen=True)
@@ -22,42 +26,32 @@ class ParameterSpec:
     converter: Callable = dataclasses.field(default=lambda x: x)
 
 
-class Parameters(IntEnum):
-    HOME_VALUE = 0
-    MONTHLY_PAYMENT = auto()
-    DOWN_PAYMENT = auto()
-    INTEREST_RATE_PCT = auto()
-    TAX_RATE_PCT = auto()
-    INSURANCE_RATE_PCT = auto()
-    TERM_MONTHS = auto()
-
-
 parameters = {
-    Parameters.HOME_VALUE: ParameterSpec(
+    MortgageParameters.HOME_VALUE: ParameterSpec(
         arg_name="home_value",
-        display_name="Home Value ($)",
-        default_value=200_000.0,
-        sweep_default_min=100_000.0,
-        sweep_default_max=1000_000.0,
+        display_name="Home Value ($k)",
+        default_value=200.0,
+        sweep_default_min=100.0,
+        sweep_default_max=1_000.0,
         sweep_default_steps=101,
     ),
-    Parameters.MONTHLY_PAYMENT: ParameterSpec(
+    MortgageParameters.MONTHLY_PAYMENT: ParameterSpec(
         arg_name="monthly_payment",
-        display_name="Monthly Payment ($)",
-        default_value=4_000.0,
-        sweep_default_min=1_000.0,
-        sweep_default_max=10_000.0,
+        display_name="Monthly Payment ($k)",
+        default_value=2.0,
+        sweep_default_min=1.0,
+        sweep_default_max=10.0,
         sweep_default_steps=101,
     ),
-    Parameters.DOWN_PAYMENT: ParameterSpec(
+    MortgageParameters.DOWN_PAYMENT: ParameterSpec(
         arg_name="down_payment",
-        display_name="Down Payment ($)",
-        default_value=40_000.0,
+        display_name="Down Payment ($k)",
+        default_value=40.0,
         sweep_default_min=0.0,
-        sweep_default_max=500_000.0,
+        sweep_default_max=500.0,
         sweep_default_steps=101,
     ),
-    Parameters.INTEREST_RATE_PCT: ParameterSpec(
+    MortgageParameters.INTEREST_RATE: ParameterSpec(
         arg_name="interest_rate",
         display_name="Interest Rate (%)",
         default_value=6.875,
@@ -67,21 +61,21 @@ parameters = {
         sweep_default_steps=101,
         converter=lambda x: x / 100,
     ),
-    Parameters.TAX_RATE_PCT: ParameterSpec(
+    MortgageParameters.TAX_RATE: ParameterSpec(
         arg_name="tax_rate",
         display_name="Tax Rate (%)",
         format_str="%.3f",
         default_value=2.0,
         converter=lambda x: x / 100,
     ),
-    Parameters.INSURANCE_RATE_PCT: ParameterSpec(
+    MortgageParameters.INSURANCE_RATE: ParameterSpec(
         arg_name="insurance_rate",
         display_name="Insurance Rate (%)",
         format_str="%.3f",
         default_value=0.5,
         converter=lambda x: x / 100,
     ),
-    Parameters.TERM_MONTHS: ParameterSpec(
+    MortgageParameters.TERM_MONTHS: ParameterSpec(
         arg_name="num_months",
         display_name="Term (Months)",
         format_str="%d",
@@ -90,15 +84,15 @@ parameters = {
 }
 
 functions = {
-    Parameters.DOWN_PAYMENT: mortgage.calc_down_payment,
-    Parameters.HOME_VALUE: mortgage.calc_home_value,
-    Parameters.MONTHLY_PAYMENT: mortgage.calc_monthly_payment,
+    MortgageParameters.DOWN_PAYMENT: calc_down_payment,
+    MortgageParameters.HOME_VALUE: calc_home_value,
+    MortgageParameters.MONTHLY_PAYMENT: calc_monthly_payment,
 }
 
-ALL_PARAMETERS = frozenset(Parameters)
+ALL_PARAMETERS = frozenset(MortgageParameters)
 
 Z_PARAMETERS = set(functions.keys())
-XY_PARAMETERS = Z_PARAMETERS | set([Parameters.INTEREST_RATE_PCT])
+XY_PARAMETERS = Z_PARAMETERS | set([MortgageParameters.INTEREST_RATE])
 
 
 def main():
@@ -124,7 +118,6 @@ def main():
             format_func=lambda x, p=parameters: p[x].display_name,
         )
         swept_parameters.remove(x_parameter)
-        print(x_parameter)
 
         y_parameter = streamlit.selectbox(
             "Y-Axis",
@@ -133,7 +126,8 @@ def main():
             format_func=lambda x, p=parameters: p[x].display_name,
         )
         swept_parameters.remove(y_parameter)
-        print(y_parameter)
+
+        scalar_swept_parameter = list(swept_parameters)[0]
 
         streamlit.write("Scalar Parameters")
         scalar_parameters = ALL_PARAMETERS - set([z_parameter, x_parameter, y_parameter])
@@ -145,8 +139,6 @@ def main():
                     config.display_name, value=config.default_value, format=config.format_str
                 )
             )
-
-        print(scalars)
 
     streamlit.header("Sweep Settings")
     min_col, max_col, num_col = streamlit.columns(3)
@@ -167,11 +159,23 @@ def main():
 
     with num_col:
         x_steps = streamlit.number_input(
-            f"{x_name} Steps", value=x_config.sweep_default_steps, max_value=101
+            f"{x_name} Steps",
+            value=x_config.sweep_default_steps,
+            min_value=2,
+            max_value=501,
+            step=1,
         )
         y_steps = streamlit.number_input(
-            f"{y_name} Steps", value=y_config.sweep_default_steps, max_value=101
+            f"{y_name} Steps",
+            value=y_config.sweep_default_steps,
+            min_value=2,
+            max_value=501,
+            step=1,
         )
+
+    if x_steps is None or y_steps is None:
+        streamlit.info("Swept parameter steps must be an integer.")
+        return 0
 
     streamlit.divider()
     streamlit.header("Results")
@@ -184,10 +188,17 @@ def main():
 
     args = {
         parameters[x_parameter].arg_name: x_sweep,
+        parameters[y_parameter].arg_name: y_sweep,
         **{parameters[s].arg_name: scalars[s] for s in scalar_parameters},
     }
 
-    z = [functions[z_parameter](**args, **{parameters[y_parameter].arg_name: y}) for y in y_sweep]
+    z, axis_parameters = functions[z_parameter](**args)
+
+    z = z.transpose(
+        axis_parameters.index(y_parameter),
+        axis_parameters.index(x_parameter),
+        axis_parameters.index(scalar_swept_parameter),
+    )
 
     fig = go.Figure()
     fig.add_traces(
@@ -195,13 +206,13 @@ def main():
             go.Heatmap(
                 x=x,
                 y=y,
-                z=z,
+                z=z[:, :, 0],
                 colorscale=pc.sequential.Viridis,
             ),
             go.Contour(
                 x=x,
                 y=y,
-                z=z,
+                z=z[:, :, 0],
                 contours={"coloring": "none", "showlabels": True},
                 showlegend=False,
                 showscale=False,
@@ -211,8 +222,8 @@ def main():
     )
     fig.update_layout(
         title={"text": parameters[z_parameter].display_name, "x": 0.5},
-        xaxis={"title": parameters[x_parameter].display_name},
-        yaxis={"title": parameters[y_parameter].display_name},
+        xaxis={"title": parameters[x_parameter].display_name, "tickprefix": "$", "ticksuffix": "k"},
+        yaxis={"title": parameters[y_parameter].display_name, "tickprefix": "$", "ticksuffix": "k"},
     )
     streamlit.plotly_chart(fig, width="content", height=600)
 
